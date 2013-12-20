4e2c79c5ec6f8858de86ebb13e836b561ed7f2a5
konir <koniroth@gmail.com>
2013-12-20 06:03:03 +0100

    Amazon Ads

--- src/com/rothconsulting/android/radiorec/AdMob.java
+++ src/com/rothconsulting/android/radiorec/AdMob.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import com.amazon.device.ads.AdLayout;
+import com.amazon.device.ads.AdRegistration;
 import com.google.ads.AdRequest;
 import com.google.ads.AdSize;
 import com.google.ads.AdView;
@@ -18,16 +20,35 @@ public class AdMob {
 			adsLayout.removeAllViews();
 			adsLayout.setVisibility(View.GONE);
 		} else {
-			// Create the adView
-			AdView adView = new AdView(context, AdSize.BANNER, Constants.ADMOB_PUBLISHER_ID);
-			Utils.log(TAG, "adView=" + adView);
-			// Lookup your LinearLayout assuming it�s been given
-			// the attribute android:id="@+id/mainLayout"
-			LinearLayout layout = (LinearLayout) context.findViewById(R.id.adsLayout);
-			// Add the adView to it
-			layout.addView(adView);
-			// Initiate a generic request to load it with an ad
-			adView.loadAd(new AdRequest());
+			showGoogleAdMobAds(context);
+			// showAmazonAds(context);
 		}
+	}
+
+	private void showGoogleAdMobAds(Activity context) {
+		// Create the adView
+		AdView adView = new AdView(context, AdSize.BANNER, Constants.ADMOB_PUBLISHER_ID);
+		Utils.log(TAG, "adView=" + adView);
+		// Lookup your LinearLayout assuming it's been given
+		// the attribute android:id="@+id/mainLayout"
+		LinearLayout layout = (LinearLayout) context.findViewById(R.id.adsLayout);
+		// Add the adView to it
+		layout.addView(adView);
+		// Initiate a generic request to load it with an ad
+		adView.loadAd(new AdRequest());
+	}
+
+	private void showAmazonAds(Activity context) {
+		AdRegistration.setAppKey(Constants.AMAZON_APPLICATION_KEY);
+		// Programmatically create the AmazonAdLayout
+		AdLayout adView = new AdLayout(context);
+		LinearLayout layout = (LinearLayout) context.findViewById(R.id.adsLayout);
+		// Set the correct width and height of the ad.
+		LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+		layout.addView(adView, lp);
+		// If you declared AdLayout in your xml you would instead
+		// replace the 3 lines above with the following line
+		// this.adView = (AdLayout) findViewById( R.id.adview );
+
 	}
 }
--- src/com/rothconsulting/android/radiorec/Constants.java
+++ src/com/rothconsulting/android/radiorec/Constants.java
@@ -174,8 +174,9 @@ public class Constants {
 	public static final String FROM_NOTIFICATION = "fromNotification";
 	public static final int FROM_FAVOURITES = 99;
 
-	public static String ADMOB_PUBLISHER_ID = "a14dc9885936cd9";
-	public static String ANALYTICS_ID = "UA-38114228-1";
+	public static final String ADMOB_PUBLISHER_ID = "a14dc9885936cd9";
+	public static final String ANALYTICS_ID = "UA-38114228-1";
+	public static final String AMAZON_APPLICATION_KEY = "363b43a633564737b350fc0f5c8096f0";
 
 	public static int CURRENT_TAB = 0;
 
